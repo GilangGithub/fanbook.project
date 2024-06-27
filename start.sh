529000855959af80a314ce520eab5f4eedf0f981
Gilang KS <gilangsholeh18@gmail.com>
2024-06-27 11:51:10 +0700

    initial commit

--- start.sh
+++ start.sh
@@ -2,18 +2,18 @@ set -eu
 
 export PYTHONUNBUFFERED=true
 
-VIRTUALENV.data/venv
+VIRTUALENV=.data/venv
 
-if [ -d SVIRTUALENV ]; then python3 -m veny SVIRTUALENV
-
+if [ ! -d $VIRTUALENV ]; then 
+    python3 -m veny SVIRTUALENV
 fi
 
-if [ -f $VIRTUALENV/bin/pip]; then curl --silent --show-error--retry 5 https://bootstrap.pypa.io/get-pip.py | SVIRTUALE
-
+if [ ! -f $VIRTUALENV/bin/pip]; then 
+    curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALE
 fi
 
-$SVIRTUALENV/bin/pip install -r requirements.txt
+$VIRTUALENV/bin/pip install -r requirements.txt
 
-$SVIRTUALENV/bin/python3 app.py
+$VIRTUALENV/bin/python3 app.py
 
 Footer
